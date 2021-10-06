61f4770e54ea5f3fc28fbf0ac81a2249554148e3
Khushbu Kumari <kumari.k@northeastern.edu>
2021-10-05 23:58:32 -0700

    Valid palindrome leetcode Q125, https://leetcode.com/problems/valid-palindrome/

--- Strings/ValidPalindrome/src/leetcode_solutions/ValidPalindrome.java
+++ Strings/ValidPalindrome/src/leetcode_solutions/ValidPalindrome.java
@@ -11,11 +11,12 @@ public class ValidPalindrome {
      * Given a strings, determine if it is a palindrome,
      * considering only alphanumeric characters and ignoring cases.
      * https://leetcode.com/problems/valid-palindrome/
+     *
      * @param s input
      * @return is palindrome true or false
      */
     public static boolean validatePalindrome(String s) {
-        if( s == null) {
+        if (s == null) {
             return true;
         }
         StringBuilder sb = new StringBuilder();
@@ -27,8 +28,8 @@ public class ValidPalindrome {
             }
         }
         String toCheck = sb.toString().toLowerCase();
-        for(int i=0, j= toCheck.length()-1; i<=j; i++,j-- ) {
-            if(toCheck.charAt(i) != toCheck.charAt(j)) {
+        for (int i = 0, j = toCheck.length() - 1; i <= j; i++, j--) {
+            if (toCheck.charAt(i) != toCheck.charAt(j)) {
                 return false;
             }
         }
@@ -41,7 +42,7 @@ public class ValidPalindrome {
         testcases.add("race a car");
         testcases.add("");
         testcases.add(null);
-        for( String testcase : testcases) {
+        for (String testcase : testcases) {
             System.out.printf("Input: %s, Is palindrome: %s%n",
                     testcase,
                     validatePalindrome(testcase));
