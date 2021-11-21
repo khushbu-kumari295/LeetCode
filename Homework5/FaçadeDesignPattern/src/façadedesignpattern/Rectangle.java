a1669938cc4eb92d3bd6ff8488122e25fa4ae939
Khushbu Kumari <kumari.k@northeastern.edu>
2021-11-21 03:48:32 -0800

    Façade design solution

--- Homework5/FaçadeDesignPattern/src/façadedesignpattern/Rectangle.java
+++ Homework5/FaçadeDesignPattern/src/façadedesignpattern/Rectangle.java
@@ -1,6 +1,6 @@
 package façadedesignpattern;
 
-public class Rectangle implements  Shape{
+public class Rectangle implements Shape {
     @Override
     public void draw() {
         System.out.println("Drawing a Rectangle ");
--- Homework5/FaçadeDesignPattern/src/façadedesignpattern/Square.java
+++ Homework5/FaçadeDesignPattern/src/façadedesignpattern/Square.java
@@ -1,6 +1,6 @@
 package façadedesignpattern;
 
-public class Square implements  Shape{
+public class Square implements Shape {
     @Override
     public void draw() {
         System.out.println("Drawing a Square ");
